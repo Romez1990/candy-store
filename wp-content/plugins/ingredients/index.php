2ba2125bd94552b9cfada487f65aa753195acfd5
Romez <Romez1990@gmail.com>
2019-11-17 23:40:11 +0300

    Create unit and coast for ingredient and display ingredients table in order

--- wp-content/plugins/ingredients/index.php
+++ wp-content/plugins/ingredients/index.php
@@ -152,3 +152,140 @@ function save_product_ingredients($post_id) {
     $json = json_encode($ingredients_array);
     update_post_meta($post_id, 'ingredients_array', $json);
 }
+
+add_action('add_meta_boxes', 'add_unit_meta_box', 1);
+function add_unit_meta_box() {
+    add_meta_box(
+        'unit_meta_box',
+        'Единица измерения',
+        'unit_meta_box',
+        'ingredients',
+    );
+}
+function unit_meta_box() {
+    global $post;
+    $saved_unit_id = get_post_meta($post->ID, 'unit')[0];
+    $units_query = new WP_Term_Query([
+        'taxonomy' => 'units',
+        'hide_empty' => false,
+    ]);
+    $units = [];
+    foreach ($units_query->get_terms() as $unit){
+        $units[$unit->term_id] = $unit->name;
+    }
+    ?>
+    <p class="form-field _select_field">
+        <label for="unit">Единица измерения</label>
+        <select name="unit" id="unit">
+            <?php
+            foreach ($units as $unit_id => $unit_name) {
+                ?>
+                <option value="<?= $unit_id ?>"
+                <?php if ($unit_id === intval($saved_unit_id)) echo 'selected'; ?>>
+                    <?= $unit_name ?>
+                </option>
+                <?php
+            }
+            ?>
+        </select>
+    </p>
+    <?php
+    woocommerce_wp_text_input( [
+       'id'                => 'coast',
+       'label'             => 'Стоимость',
+       'placeholder'       => 'Стоимость',
+       'type'              => 'number',
+       'custom_attributes' => [
+          'min' => '0',
+       ],
+    ]);
+}
+
+add_action('save_post', 'save_unit', 10);
+
+function save_unit($post_id) {
+    $unit_id = $_POST['unit'];
+    $coast = $_POST['coast'];
+    if (isset($unit_id))
+        update_post_meta($post_id, 'unit', $unit_id);
+    if (isset($coast))
+        update_post_meta($post_id, 'coast', $coast);
+}
+
+add_action('add_meta_boxes', 'add_ingredients_meta_box', 1);
+function add_ingredients_meta_box() {
+    add_meta_box(
+        'ingredients_meta_box',
+        'Ингридиенты',
+        'ingredients_meta_box',
+        'shop_order',
+    );
+}
+
+function ingredients_meta_box($order) {
+    $order = wc_get_order($order->ID);
+    $items = $order->get_items();
+    $ingredients = [];
+    $total = 0;
+    foreach ($items as $item) {
+        $product_id = $item['product_id'];
+        $product_ingredients = json_decode(get_post_meta($product_id, 'ingredients_array')[0]);
+        foreach($product_ingredients as $ingredient_id => $amount) {
+            $amount = (float) str_replace (',', '.', $amount);
+            if (!isset($ingredients[$ingredient_id]))
+                $ingredients[$ingredient_id] = $amount;
+            else
+                $ingredients[$ingredient_id] += $amount;
+        }
+    }
+    ?>
+    <style>
+        .ingredients-table td .ingredient-name,
+        .ingredients-table tr .ingredient-name {
+            text-align: left;
+        }
+
+        .ingredients-table td:not(.ingredient-name),
+        .ingredients-table tr:not(.ingredient-name) {
+            text-align: right;
+        }
+
+    </style>
+    <table class="ingredients-table" width="100%">
+        <thead>
+            <tr>
+                <th class="ingredient-name" width="55%">Ингридиент</th>
+                <th class="cost" width="15%">Стоимость</th>
+                <th class="quantity" width="15%">Кол-во</th>
+                <th class="total-cost" width="15%">Итого</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($ingredients as $ingredient_id => $amount) {
+                $ingredient_name = get_the_title($ingredient_id);
+                $ingredient_coast = get_post_meta($ingredient_id, 'coast')[0];
+                $unit_id = get_post_meta($ingredient_id, 'unit')[0];
+                $unit_name = get_term($unit_id)->name;
+                $total_coast = $ingredient_coast * $amount;
+                $total += $total_coast;
+                ?>
+                <tr>
+                    <td class="ingredient-name"><?= $ingredient_name ?></td>
+                    <td class="cost"><?= $ingredient_coast ?>&nbsp;₽</td>
+                    <td class="quantity"><?= $amount ?> <?= $unit_name ?></td>
+                    <td class="total-cost"><?= $total_coast ?> ₽</td>
+                </tr>
+                <?php
+            }
+            ?>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Итого:</td>
+                <td><?= $total ?> ₽</td>
+            </tr>
+            </tbody>
+        </table>
+    <?php
+}
